d75249c00c2a0b17ad4e2ab9bab5e45b60f9a655
caoa <gnargu@gmail.com>
2019-05-19 18:27:15 +0200

    Fix API description

--- src/main/java/org/alexiscao/bank/AccountApi.java
+++ src/main/java/org/alexiscao/bank/AccountApi.java
@@ -14,7 +14,7 @@ public interface AccountApi {
      * Make a deposit, ie *decrease* the current balance of the account by the given amount. The amount MUST be positive.
      *
      * @param amount amount of money to remove
-     * @throws IllegalArgumentException if the amount is negative
+     * @throws IllegalArgumentException if the amount is negative or the balance is lower than the amount
      */
     void withdraw(int amount);
 
